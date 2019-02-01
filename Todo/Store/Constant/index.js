2d4a7020aed7bd363babd52db2ada27541ea1c4d
Fahad Memon <42487543+fahadyahya14@users.noreply.github.com>
2019-02-01 02:14:46 -0800

    Create index.js

---
+++ Todo/Store/Constant/index.js
@@ -0,0 +1,16 @@
+const ActionTypes = {
+    USERNAME: 'USERNAME',
+    TODO: 'TODO',
+    DELETETODO: 'DELETETODO',
+    EDITTODO: 'EDITTODO',
+    UPDATETODO: 'UPDATETODO',
+    CANCEL: 'CANCEL',
+    DELETEALLTODO : "DELETEALLTODO",
+    LoginSuccessFull:"LoginSuccessFull",
+    isSignfalse:"isSignfalse",
+    isSignture:"isSignture",ForData:'ForData',
+    ReduxAddTodo:'ReduxAddTodo',Error:"Error",Free:"Free"
+
+}
+
+export default ActionTypes;
